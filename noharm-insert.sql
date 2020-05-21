62209aa5dbee1bc902f75fd358fd3d050607c01e
Henrique Dias <heukirne@gmail.com>
2020-05-21 17:21:55 -0300

    bug fix

--- noharm-insert.sql
+++ noharm-insert.sql
@@ -1062,7 +1062,7 @@ INSERT INTO demo.unidadeconverte (fkhospital,fkunidademedida,fator,fkmedicamento
 ,(1,'3',3,2)
 ,(1,'1',1,2);
 
-INSERT INTO demo.intervencao (fkpresmed, nratendimento, update_by, idmotivointervencao, tipo, observacao) VALUES
+INSERT INTO demo.intervencao (fkpresmed, nratendimento, update_by, idmotivointervencao, status, observacao) VALUES
 	(4, 1, 1, ARRAY[2], 'N', '<p><strong>Existe </strong><em>apresenta&ccedil;&atilde;o adequada</em> de <strong>50mg</strong></p>'),
 	(18, 2, 1, ARRAY[3], 'N', 'Possivel erro de via. Trocar para EV'),
 	(14, 3, 1, ARRAY[2], 'N', 'Frequência potencialmente inadequada para paciente obeso.'),
