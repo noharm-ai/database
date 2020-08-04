ebe6325ad422cfcf9c2d3ea47c7b7f3ddaf33c26
Augusto Dias <gutosantos82@users.noreply.github.com>
2020-08-04 18:59:42 +1000

    Remove idade from completa_prescricaoagg

--- noharm-triggers.sql
+++ noharm-triggers.sql
@@ -290,9 +290,9 @@ AS $BODY$BEGIN
    IF pg_trigger_depth() = 1 then
 
         INSERT INTO demo.prescricaoagg
-            (fkhospital, fksetor, fkmedicamento, fkunidademedida, fkfrequencia, dose, frequenciadia, idade, peso, contagem, doseconv)
-            VALUES(1, NEW.fksetor, NEW.fkmedicamento, NEW.fkunidademedida, NEW.fkfrequencia, NEW.dose, NEW.frequenciadia, NEW.idade, NEW.peso, NEW.contagem, NEW.doseconv)
-        ON CONFLICT (fksetor, fkmedicamento, fkunidademedida, fkfrequencia, dose, frequenciadia, idade, peso)
+            (fkhospital, fksetor, fkmedicamento, fkunidademedida, fkfrequencia, dose, frequenciadia, peso, contagem, doseconv)
+            VALUES(1, NEW.fksetor, NEW.fkmedicamento, NEW.fkunidademedida, NEW.fkfrequencia, NEW.dose, NEW.frequenciadia, NEW.peso, NEW.contagem, NEW.doseconv)
+        ON CONFLICT (fksetor, fkmedicamento, fkunidademedida, fkfrequencia, dose, frequenciadia, peso)
          DO UPDATE SET contagem = NEW.contagem, doseconv = NEW.doseconv, idsegmento = NEW.idsegmento, frequenciadia = NEW.frequenciadia;
 
       RETURN NULL;
