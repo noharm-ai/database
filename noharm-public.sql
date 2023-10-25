f39c89b38123d38f3f27e0fbbf000751cf76ee9d
Marcelo Arocha <marceloarocha@gmail.com>
2023-10-24 21:33:55 -0300

    active substance

--- noharm-public.sql
+++ noharm-public.sql
@@ -13,7 +13,8 @@ CREATE TABLE public."substancia" (
   "sctid" bigint NOT NULL,
   "nome" varchar(255) NOT NULL,
   "link" varchar(255) DEFAULT NULL,
-  "idclasse" varchar(10) NULL
+  "idclasse" varchar(10) NULL,
+  "ativo" boolean DEFAULT true
 );
 
 CREATE TABLE public."notifica" (
@@ -22,7 +23,7 @@ CREATE TABLE public."notifica" (
   "tooltip" varchar(255) NOT NULL,
   "link" varchar(100) NOT NULL,
   "icon" varchar(25) NOT NULL,
-  "classname"  varchar(50) NOT NULL,
+  "classname" varchar(50) NOT NULL,
   "inicio" date NOT NULL,
   "validade" date NOT NULL,
   "schema" varchar(25) NULL
@@ -40,26 +41,27 @@ CREATE TABLE public."relacao" (
 );
 
 CREATE TABLE public.bulletin (
-	idbulletin serial4 NOT NULL,
-	"schema" varchar(50) NULL,
-	category text NULL,
-	groupid text NULL,
-	id text NULL,
-	"level" text NULL,
-	message text NULL,
-	sourcename text NULL,
-	sourceid text NULL,
-	"timestamp" text NULL,
-	created_at timestamptz NULL DEFAULT now(),
-	CONSTRAINT bulletin_pkey PRIMARY KEY (idbulletin)
+  idbulletin serial4 NOT NULL,
+  "schema" varchar(50) NULL,
+  category text NULL,
+  groupid text NULL,
+  id text NULL,
+  "level" text NULL,
+  message text NULL,
+  sourcename text NULL,
+  sourceid text NULL,
+  "timestamp" text NULL,
+  created_at timestamptz NULL DEFAULT now(),
+  CONSTRAINT bulletin_pkey PRIMARY KEY (idbulletin)
 );
 
 CREATE TABLE public.classe (
-	idclasse varchar(10) NOT NULL,
-	idclassemae varchar(10) NULL,
-	nome varchar(250) NOT NULL,
-	CONSTRAINT classe_pkey PRIMARY KEY (idclasse)
+  idclasse varchar(10) NOT NULL,
+  idclassemae varchar(10) NULL,
+  nome varchar(250) NOT NULL,
+  CONSTRAINT classe_pkey PRIMARY KEY (idclasse)
 );
 
 CREATE UNIQUE INDEX ON public."substancia" ("sctid");
-CREATE UNIQUE INDEX ON public."relacao" ("sctida", "sctidb","tprelacao");
+
+CREATE UNIQUE INDEX ON public."relacao" ("sctida", "sctidb", "tprelacao");
