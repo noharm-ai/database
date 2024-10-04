4c3132c61d3f82d5bea05ba74c357b058e4d3ee0
Henrique Dias <heukirne@gmail.com>
2024-10-04 14:41:46 -0300

    Update noharm-create.sql

--- noharm-create.sql
+++ noharm-create.sql
@@ -157,7 +157,7 @@ CREATE TABLE demo."prescricao" (
   "tp_revisao" smallint default 0,
   "update_at" timestamp DEFAULT NOW(),
   "update_by" integer,
-  "dtcriacao_origem" timestamp default null
+  "dtcriacaoorigem" timestamp default null
 );
 
 create table demo."prescricao_audit" (
