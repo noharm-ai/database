6c50c3b959068f0540bef16610e2a29dfb2ad96e
Júlia Colleoni Couto <34305916+juliacolleoni@users.noreply.github.com>
2025-06-11 15:08:35 -0300

    Update first line

--- noharm-newuser.sql
+++ noharm-newuser.sql
@@ -1,4 +1,4 @@
---> Quando for PEC, não criar user individual, comentar as três primeiras linhas e alterar demo_user para pec_user, criar apenas schema (sem getname, logstream, fila sqs). Comentar linhas 60-62 (getname).
+--> Quando for PEC, não criar user individual, comentar as três primeiras linhas e alterar demo_user para pec_user, criar apenas schema (sem getname, logstream, fila sqs). Comentar linhas 68-70 (getname).
 CREATE USER demo_user WITH PASSWORD 'demo_user' CONNECTION LIMIT 10;
 ALTER ROLE demo_user SET statement_timeout = 10000;
 GRANT CONNECT ON DATABASE postgres TO demo_user;
